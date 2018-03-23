a90d82aff357510308a927f9d61cb365dfaa8538
StefanSebastian <stefansebii@gmail.com>
2018-03-23 16:48:20 +0200

    Conditional of Vandermonde

---
+++ lab3/prob3/subpctb.m
@@ -0,0 +1,17 @@
+% subpct b
+x = zeros(1, 6);
+y = zeros(1, 6);
+for n = 10:15
+    t = zeros(1, n);
+    for k = 1:n
+        t(k) = 1 / k;
+    end
+   
+    v = vander(t);
+    c = cond(v, 'inf')
+    
+    x(n - 9) = n;
+    y(n - 9) = c;
+end
+
+scatter(x, y)
