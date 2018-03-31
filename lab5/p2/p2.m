37615fdcb473309e8a78fd1262ed4c3762ff8dac
StefanSebastian <stefansebii@gmail.com>
2018-03-31 13:07:13 +0300

    omega optim

--- lab5/p2/p2.m
+++ lab5/p2/p2.m
@@ -1,2 +1,8 @@
 solveGS([2 1; 5 7], [4; 19], 0.1)
 solveSOR([2 1; 5 7], [4; 19], 0.1, 1)
+
+% omega optim
+A = [2 1; 5 7];
+ro = max(abs(eig(A))); % https://www.mathworks.com/matlabcentral/answers/378909-spectral-radius-of-matrix?s_tid=al_cta2
+omega = 2 / (1 + sqrt(1 - ro^2))
+solveSOR(A, [4; 19], 0.1, omega)
